0a73a11029c62c72305aef0fc53c434a309d8b3e
ROHIT <rohityadav3461@gmail.com>
2026-04-16 18:48:37 +0530

    add dotenv file

--- dmas-frontend/src/api/sosService.ts
+++ dmas-frontend/src/api/sosService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import type { HelpRequestDTO, UserDTO } from '../types/helpRequest';
 
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/api`;
 
 // 1. Create an axios instance
 const api = axios.create({
